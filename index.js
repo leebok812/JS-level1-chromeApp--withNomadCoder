17c98ef1d70abc60186e8a6bd495bd34a908d68a
JUNEBOK <leebok0812@gmail.com>
2021-02-11 09:48:22 +0900

    Your first JS Variable(변수!)

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert('Im working. Im JS. Im Beautiful. Im worth it');
+{
+    alert('Im working. Im JS. Im Beautiful. Im worth it');
+}
+
+{ ////////// Your first JS Variable(변수!)
+     
+     let a = 221; //a를 생성하고 숫자 221로 초기화한것.
+     let b = a -5; // 여기 a에 let을 쓰지 않은 이유는 위에서 생성된 a를 참조 하기 때문이다. 즉, 같은a라는 것.
+    a = 4 ;  // 위에서 선언한 a를 다시 4로 업데이트 해줌.
+     console.log(b,a);
+
+
+}
