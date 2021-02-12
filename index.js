c4c4d05efa75ff7ae4dd91451bc87e509d5a6844
JUNEBOK <leebok0812@gmail.com>
2021-02-12 10:14:40 +0900

    let, const, var

--- index.js
+++ index.js
@@ -2,12 +2,32 @@
     alert('Im working. Im JS. Im Beautiful. Im worth it');
 }
 
-{ ////////// Your first JS Variable(변수!)
-     
+{ ////////// 1. Your first JS Variable(변수!)
+     console.log("===================1번===================");
      let a = 221; //a를 생성하고 숫자 221로 초기화한것.
      let b = a -5; // 여기 a에 let을 쓰지 않은 이유는 위에서 생성된 a를 참조 하기 때문이다. 즉, 같은a라는 것.
     a = 4 ;  // 위에서 선언한 a를 다시 4로 업데이트 해줌.
      console.log(b,a);
+
+
+}
+
+{////////////// let, const, var
+    console.log("===================2번===================");
+    const a = 221; //const는 상수(constant) 변하지 않음!
+    let b = a - 5; // 여기 a에 let을 쓰지 않은 이유는 위에서 생성된 a를 참조 하기 때문이다. 즉, 같은a라는 것.
+   //a = 4 ;  // 위에서 선언한 a를 다시 4로 업데이트하려 했으나 위에 a를  const로 지정했기에 오류발생!
+   var c = a + b;
+   
+    b = 7;
+
+   c = 5; // var도 let 처럼  변환가능!
+   
+   
+   console.log(a,b,c);
+
+
+
 
 
 }
